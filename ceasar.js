d6046326ff5982424fe36e0459ab69678afac0a6
Zmulvihill <zmulvihi@yahoo.com>
2023-04-16 15:10:41 -0700

    added pseudocode to ceasercipher

---
+++ ceasar.js
@@ -0,0 +1,13 @@
+function rot13(str) {
+    //function rot13(inputString):
+      //decodedString = ""
+      //for each character in inputString:
+         // if character is an alphabetic character:
+             // apply ROT13 cipher to character
+         // else:
+             // add character to decodedString
+      //return decodedString
+    return str;
+  }
+  
+  console.log(rot13("SERR PBQR PNZC"));
