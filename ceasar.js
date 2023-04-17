e63a2a37f317f9e87fb2e9579c0284798009023b
Zmulvihill <zmulvihi@yahoo.com>
2023-04-17 10:36:17 -0700

    added comments to code to help better understand

--- ceasar.js
+++ ceasar.js
@@ -1,16 +1,16 @@
 function rot13(str) {
-  let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  let answer = "";
-  for (let i = 0; i < str.length; i++) {
-    if (alphabet.indexOf(str[i]) >= 13) {
-      answer += alphabet[alphabet.indexOf(str[i]) - 13];
-    } else if (alphabet.indexOf(str[i]) < 13 && alphabet.indexOf(str[i]) > -1) {
-      answer += alphabet[alphabet.indexOf(str[i]) + 13];
-    } else {
-      answer += str[i];
+  let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";  // Define the alphabet as a string of uppercase letters
+  let answer = "";  // Initialize an empty string to hold the decoded message
+  for (let i = 0; i < str.length; i++) { // Loop through each character in the input string
+    if (alphabet.indexOf(str[i]) >= 13) { // If the current character is in the second half of the alphabet
+      answer += alphabet[alphabet.indexOf(str[i]) - 13];  // Shift the character back 13 places and add it to the answer string
+    } else if (alphabet.indexOf(str[i]) < 13 && alphabet.indexOf(str[i]) > -1) { // Otherwise, if the current character is in the first half of the alphabet
+      answer += alphabet[alphabet.indexOf(str[i]) + 13];  // Shift the character forward 13 places and add it to the answer string
+    } else {  // Otherwise, if the current character is not a letter
+      answer += str[i]; // Add the character to the answer string unchanged
     }
   }
-  return answer;
+  return answer;  // Return the decoded message
 }
 
 console.log(rot13("SERR PBQR PNZC"))
